30ed892b3f7303ad3d3b207e2a573246b8e375c7
Nic-Wolf <azalraku@gmail.com>
2018-01-16 15:40:01 -0800

    starting work on session 5

---
+++ Session_5/session_5.js
@@ -0,0 +1,11 @@
+// Given what we know thus far about objects
+
+// Consider the following object:
+function Thing() {
+    this._thing = '';
+
+    this.print = function() {
+        console.log();
+    };
+
+}
