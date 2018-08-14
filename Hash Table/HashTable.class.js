50dbec2b1123c46f4ff7fa1ca66f9d2b9c4288e4
Nic-Wolf <azalraku@gmail.com>
2018-08-14 15:22:54 -0700

    adding a setter

--- Hash Table/HashTable.class.js
+++ Hash Table/HashTable.class.js
@@ -3,8 +3,35 @@ class HashTable {
         this.inaneValue = 'test';
     }
 
-    inaneMethod(value) {
-        return this[value];
+    set(key, val, type) {
+        if (!this.hasOwnProperty(key) || Object.getOwnPropertyDescriptor(this, key).writable) {
+            switch (type) {
+                case 'static':
+                    Object.defineProperty(this, key, {
+                        value: val,
+                        writable: false,
+                        configurable: false
+                    });
+                    break;
+                default:
+                    // Set everything else to public.
+                    this[key] = val;
+            }
+        } else {
+            console.log(`The parameter: '${key}' has already been assigned to this instance.`);
+        }
+    }
+
+    get(key) {
+        return this[key];
+    }
+
+    delete(key) {
+        if (this.hasOwnProperty(key)) {
+            delete this[key];
+        } else {
+            console.log('Nothing to delete.');
+        }
     }
 
 }
