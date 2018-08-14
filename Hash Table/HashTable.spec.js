7f2817822e7b08a88b36334323a4ec641e37817d
Nic-Wolf <azalraku@gmail.com>
2018-08-14 12:39:12 -0700

    setting up assert object

--- Hash Table/HashTable.spec.js
+++ Hash Table/HashTable.spec.js
@@ -5,18 +5,21 @@ function runTests() {
     // Test: init class
     const options = {};
     const hashTable = new HashTable(options);
-    // Assertion
-    const test1 = objIsEquivalent(hashTable, new HashTable()) ?
-        {text: 'Success - Hash Table initialized.', pass: true} :
-        {text: 'Fail - Could not construct Hash Table.', pass: false };
-    results.push({ result: test1, data: `${hashTable.constructor.name}: ${JSON.stringify(hashTable)}` });
+    // // Assertion
+    // const test1 = objIsEquivalent(hashTable, new HashTable()) ?
+    //     {text: 'Success - Hash Table initialized.', pass: true} :
+    //     {text: 'Fail - Could not construct Hash Table.', pass: false };
+
+    const test1 = new Assertion(`Initializing hashTable as HashTable.`, hashTable);
+    test1.test(objIsEquivalent(hashTable, new HashTable()));
+    results.push(test1);
 
 
     // Test: add entry
     // const aKey = 'thing';
     // const aValue = 12345;
     // hashTable.insert(aKey, aValue);
-    // Assertion
+    // // Assertion
     // const expectedData = Object.assign({}, { [aKey]: aValue });
     // const test2 = objIsEquivalent(hashTable[aKey], expectedData) ?
     //     'Success - Entry added to Hash Table.' :
@@ -42,6 +45,36 @@ function runTests() {
     return results;
 }
 
+class Assertion {
+    constructor(testStr, data) {
+        this.testStr = testStr;
+        this.data = data;
+
+        this.setDetails(this.data);
+    }
+
+    setDetails(data) {
+        if (typeof data === 'object') {
+            this.details = `${data.constructor.name}: ${JSON.stringify(data)}`
+        } else {
+            this.details = 'not yet implemented.';
+        }
+    }
+
+    test(callback) {
+        this.pass = callback;
+    }
+
+    // Assertion
+    dataSameAs(val) {
+        return objIsEquivalent(this.data, val);
+    }
+
+    dataHasParam(key, val) {
+        const expectedData = Object.assign({}, { [key]: val });
+        return objIsEquivalent(this.data[key], expectedData);
+    }
+}
 
 function objIsEquivalent(a, b) {
     const aProps = Object.getOwnPropertyNames(a);
