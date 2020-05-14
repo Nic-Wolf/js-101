7c62f2293f31383dfb6f9551d5d53c7392f6ca5f
Nic <azalraku@gmail.com>
2020-05-14 13:43:46 -0700

    "any" types

--- TypeScript/example_2.ts
+++ TypeScript/example_2.ts
@@ -1,27 +1,32 @@
 // Type Annotations
 
-// Primitives
+// Primitives - annotation is generally not needed. 
 let someValue: number = 5;
 let firstName: string = "Bobby";
 let skyIsBlue: boolean = true;
 let surelyNot: null = null;
 let nothingYet: undefined = undefined;
 
-// Built-ins
-let rigthNot: Date = new Date();
+// Built-ins - annotation is generally not needed. 
+let rightNow: Date = new Date();
 
-// Arrays
+// Arrays - annotation is sometimes a good idea
 let colors: string[] = ['red', 'blue', 'green'];
 let values: number[] = [1, 2, 3];
 let truths: boolean[] = [true, false, false];
 
-// Object Literals
+// Object Literals - annotation is generally a good idea.
 const person: {name: string; age:number} = {
     name: 'Bobby',
     age: 10
 };
 
-// Functions
+// Functions-  annotation is recommended.
 const doThings: (val: number) => string = (val) => { 
     return `a string: ${val}`;
 };
+
+// "Any" types - always try to annotate.
+const jsonString = '{"x": 10, "y": 20}';
+const coord: {x:number; y:number} = JSON.parse(jsonString); // No error, but parse returns "any", which can't be evaluated for issues.
+
