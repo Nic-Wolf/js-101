3b45a1372c8f5eb195f65ed148aed358f610bba7
Nic <azalraku@gmail.com>
2020-05-18 11:54:48 -0700

    Extending classes and inheritance

--- TypeScript/example_6.ts
+++ TypeScript/example_6.ts
@@ -2,10 +2,24 @@
 
 class Vehicle {
     drive(): void {
-        console.log('Car is in motion...');
+        console.log('moving...');
+    }
+    honk(): void {
+        console.log('HONK');
     }
 }
 
-const vehicle = new Vehicle();
+// Method Overrides...
+class Car extends Vehicle {
+    drive(): void {
+        console.log('Eurobeat Intensifies...');
+    } 
+}
 
+const vehicle = new Vehicle();
 vehicle.drive();
+vehicle.honk();
+
+const car = new Car();
+car.drive();
+car.honk();
